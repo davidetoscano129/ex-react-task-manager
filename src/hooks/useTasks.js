33cc634c5d1ca3a72d9625af4c6c924dd0f52f2c
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-06 16:13:06 +0200

    Milestone 4 completed

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from "react";
+const { VITE_API_URL } = import.meta.env;
+
+export default function useTasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch(`${VITE_API_URL}/tasks`)
+      .then((res) => res.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const addTask = () => {};
+
+  const removeTask = () => {};
+
+  const updateTask = () => {};
+
+  return { tasks, addTask, removeTask, updateTask };
+}
