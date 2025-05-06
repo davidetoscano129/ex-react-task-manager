bce755547fc8279f5ee3a58f1fb0ac3ccee05989
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-06 11:48:03 +0200

    Milestone 3 completed

---
+++ src/components/TaskRow.jsx
@@ -0,0 +1,36 @@
+import { memo } from "react";
+
+const TaskRow = memo(({ task }) => {
+  // Styling for the status cell
+  const getStatusStyle = (status) => {
+    switch (status) {
+      case "To do":
+        return {
+          backgroundColor: "red",
+          color: "white",
+        };
+      case "Doing":
+        return {
+          backgroundColor: "yellow",
+          color: "black",
+        };
+      case "Done":
+        return {
+          backgroundColor: "green",
+          color: "white",
+        };
+      default:
+        return {};
+    }
+  };
+
+  return (
+    <tr>
+      <td>{task.title}</td>
+      <td style={getStatusStyle(task.status)}>{task.status}</td>
+      <td>{new Date(task.createdAt).toLocaleDateString()}</td>
+    </tr>
+  );
+});
+
+export default TaskRow;
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
+import TaskRow from "../components/TaskRow";
 
 export default function TaskList() {
   const { tasks } = useContext(GlobalContext);
@@ -9,7 +10,20 @@ export default function TaskList() {
     <>
       <div>
         <h1>Lista dei Task</h1>
-        <p>Qui vengono visualizzate le task</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Status</th>
+              <th>Data di creazione</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <TaskRow key={task.id} task={task} />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
