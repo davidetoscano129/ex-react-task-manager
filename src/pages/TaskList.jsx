43a6d9c5364b3bde4b74c4061fc44260723d2b38
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-12 16:14:26 +0200

    Milestone 12 completed

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,10 +1,28 @@
-import { useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import TaskRow from "../components/TaskRow";
+
+// Funzione di debounce generica
+function debounce(callback, delay) {
+  let timer;
+
+  return (value) => {
+    clearTimeout();
+    timer = setTimeout(() => {
+      callback(value);
+    }, delay);
+  };
+}
 
 export default function TaskList() {
   const { tasks } = useContext(GlobalContext);
   console.log("Task:", tasks);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedSetSearchQuery = useCallback(
+    debounce(setSearchQuery, 500),
+    []
+  );
 
   const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
@@ -20,31 +38,41 @@ export default function TaskList() {
     }
   };
 
-  const sortedTask = useMemo(() => {
-    return [...tasks].sort((a, b) => {
-      let comparison;
+  const filteredAndSortedTasks = useMemo(() => {
+    return [...tasks]
+      .filter((t) => t.title.toLowerCase().includes(searchQuery.toLowerCase()))
+      .sort((a, b) => {
+        let comparison;
 
-      if (sortBy === "title") {
-        comparison = a.title.localeCompare(b.title);
-      } else if (sortBy === "status") {
-        const statusOptions = ["To Do", "Doing", "Done"];
-        const statusA = statusOptions.indexOf(a.status);
-        const statusB = statusOptions.indexOf(b.status);
-        comparison = statusA - statusB;
-      } else if (sortBy === "createdAt") {
-        const dateA = new Date(a.createdAt).getTime();
-        const dateB = new Date(b.createdAt).getTime();
-        comparison = dateA - dateB;
-      }
+        if (sortBy === "title") {
+          comparison = a.title.localeCompare(b.title);
+        } else if (sortBy === "status") {
+          const statusOptions = ["To Do", "Doing", "Done"];
+          const statusA = statusOptions.indexOf(a.status);
+          const statusB = statusOptions.indexOf(b.status);
+          comparison = statusA - statusB;
+        } else if (sortBy === "createdAt") {
+          const dateA = new Date(a.createdAt).getTime();
+          const dateB = new Date(b.createdAt).getTime();
+          comparison = dateA - dateB;
+        }
 
-      return comparison * sortOrder;
-    });
-  }, [tasks, sortBy, sortOrder]);
+        return comparison * sortOrder;
+      });
+  }, [tasks, sortBy, sortOrder, searchQuery]);
 
   return (
     <>
       <div>
         <h1>Lista dei Task</h1>
+
+        {/* Input di ricerca */}
+        <input
+          type="text"
+          placeholder="Cerca una task..."
+          onChange={(e) => debouncedSetSearchQuery(e.target.value)}
+        />
+
         <table>
           <thead>
             <tr>
@@ -60,7 +88,7 @@ export default function TaskList() {
             </tr>
           </thead>
           <tbody>
-            {sortedTask.map((task) => (
+            {filteredAndSortedTasks.map((task) => (
               <TaskRow key={task.id} task={task} />
             ))}
           </tbody>
